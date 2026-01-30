6e3d1842dc7d6426eee570e03020446db8962f8b
BenWassa <haddonbenjamin@gmail.com>
2026-01-30 11:09:33 -0500

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/morpheus-dream-archive/',
+  base: '/template-morpheus/',
   build: {
     outDir: 'docs',
   },
